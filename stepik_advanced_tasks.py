2d6a131ba3cfd02c0ecac6d9122fe8731de45705
Kollp <muhammadaminmazitov@gmail.com>
2026-07-01 18:43:16 +0300

    Add first advanced tasks

---
+++ stepik_advanced_tasks.py
@@ -0,0 +1,19 @@
+# Задача 1: Математический принт
+# a, b = int(input()), int(input())
+# print(a + b, a - b, a * b, a / b, a // b, a % b, (a ** 10 + b ** 10) ** 0.5, sep="\n")
+
+# Задача 2: Вычисление ИМТ
+# Weight = float(input())
+# Height = float(input())
+# IMT = Weight / (Height * Height)
+# if IMT < 18.5:
+#     print("Недостаточная масса")
+# elif IMT > 25:
+#     print("Избыточная масса")
+# else:
+#     print("Оптимальная масса")
+
+
+# Задача 3: Стоимость строки
+# text = len(input())
+# print(f"{(text * 60) // 100} р. {(text * 60) % 100} коп.")
