ead0033df076413588c20d8356a437b389ef7deb
Kollp <muhammadaminmazitov@gmail.com>
2026-07-03 21:10:06 +0300

    Solve Pascal triangle task using function and return

--- nested_lists.py
+++ nested_lists.py
@@ -1,3 +1,16 @@
 # Задача 1:
 # flat_list = [num for sub_list in list1 for num in sub_list]
 # print(sum(flat_list) / len(flat_list))
+
+# Задача 2: Треугольник Паскаля 1 🌶️
+# def pascal(n):
+#     row = [1]
+#     for _ in range(n):
+#         next_row = [1]
+#         for i in range(len(row) - 1):
+#             next_row.append(row[i] + row[i + 1])
+#         next_row.append(1)
+#         row = next_row
+#     return row
+# n = int(input())
+# print(pascal(n))
