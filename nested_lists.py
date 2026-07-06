f4ed7c05ea6992a4ff1e32ed585791aebf083dca
Kollp <muhammadaminmazitov@gmail.com>
2026-07-06 22:41:14 +0300

    Победил финального босса: спираль и матрицы пройдены 🐌🌶️

--- nested_lists.py
+++ nested_lists.py
@@ -101,3 +101,33 @@
 #         print("YES")
 #     else:
 #         print("NO")
+
+# Задача 8: Заполнение спиралью 🌀🌶️🌶️
+# n, m = map(int, input().split())
+# matrix = [[0] * m for _ in range(n)]
+# current_num = 1
+# top = 0
+# bottom = n - 1
+# left = 0
+# right = m - 1
+# while current_num <= (n * m):
+#     for j in range(left, right + 1):
+#         matrix[top][j] = str(current_num).ljust(3)
+#         current_num += 1
+#     top += 1
+#     for i in range(top, bottom + 1):
+#         matrix[i][right] = str(current_num).ljust(3)
+#         current_num += 1
+#     right -= 1
+#     if top <= bottom:
+#         for j in range(right, left - 1, -1):
+#             matrix[bottom][j] = str(current_num).ljust(3)
+#             current_num += 1
+#         bottom -= 1
+#     if left <= right:
+#         for i in range(bottom, top - 1, -1):
+#             matrix[i][left] = str(current_num).ljust(3)
+#             current_num += 1
+#         left += 1
+# for row in matrix:
+#     print(*row)
