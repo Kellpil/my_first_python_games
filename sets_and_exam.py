be2a080012ef0b7b0e5f1da856fc57fabf28aa9c
Kollp <muhammadaminmazitov@gmail.com>
2026-07-08 13:35:09 +0300

    Уничтожил раздел множеств и КР! На счётчике ровно 1000 задач! 🎯🚀🔥

---
+++ sets_and_exam.py
@@ -0,0 +1,21 @@
+# Задача 1: Вершина параболы
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# x = -b / (2 * a)
+# y = (4 * a * c - b ** 2) / (4 * a)
+# vertex = (x, y)
+# print(vertex)
+
+# Задача 3: Последовательность Трибоначчи
+# n = int(input())
+# f1, f2, f3 = 1, 1, 1
+# if n == 1:
+#     print(1)
+# elif n == 2:
+#     print(1, 1)
+# else:
+#     print(f1, f2, f3, end=" ")
+#     for _ in range(n - 3):
+#         f1, f2, f3 = f2, f3, f1 + f2 + f3
+#         print(f3, end=" ")
